2e4dc0adf58f0dedf2a104a52e313990e0608539
Pedro Macias <pin.macias@gmail.com>
2021-04-04 21:32:23 +0200

    creando los templates

--- refugioApp/views.py
+++ refugioApp/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
+    return render (request,'refugioApp/home.html')
 
 def servicios(request):
-    return HttpResponse('Servicios')
+    return render (request,'refugioApp/servicios.html')
 
 def refugio(request):
-    return HttpResponse('Refugio')
+    return render (request,'refugioApp/refugio.html')
 
 def blog(request):
-    return HttpResponse('Blog')
+    return render (request,'refugioApp/blog.html')
 
 def contacto(request):
-    return HttpResponse('Contacto')
+    return render (request,'refugioApp/contacto.html')
