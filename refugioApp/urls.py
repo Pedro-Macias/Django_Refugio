c7d61dc49ac6c43bb0220f9c531dc5d60ae68eed
Pedro Macias <pin.macias@gmail.com>
2021-04-05 12:43:46 +0200

    plantilla padre y heredamos de las demás

--- refugioApp/urls.py
+++ refugioApp/urls.py
@@ -6,7 +6,7 @@ from refugioApp import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('servicios', views.servicios, name='Servicos'),
+    path('servicios', views.servicios, name='Servicios'),
     path('refugio', views.refugio, name='Refugio'),
     path('blog', views.blog, name='Blog'),
     path('contacto', views.contacto, name='Contacto'),
