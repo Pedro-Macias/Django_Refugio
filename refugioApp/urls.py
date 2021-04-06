450200aed8479a858ae96fc6cdf9d6a8363ea6f9
Pedro Macias <pin.macias@gmail.com>
2021-04-06 21:02:24 +0200

    subir y ver imágenes de aplicación servicios

--- refugioApp/urls.py
+++ refugioApp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 
 # importamos las vistas
 from refugioApp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='Home'),
@@ -11,3 +12,6 @@ urlpatterns = [
     path('blog', views.blog, name='Blog'),
     path('contacto', views.contacto, name='Contacto'),
 ]
+
+# indicar la url para ver las imagenes 
+urlpatterns += static(settings.MEDIA_URL , document_root= settings.MEDIA_ROOT)
--- serviciosApp/models.py
+++ serviciosApp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Servicio(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=100)
-    imagen = models.ImageField()
+        # con el parametro upload_to , le indicamos que las imagenes las guarde en media en la carpeta servicios
+    imagen = models.ImageField(upload_to='servicios')
     created = models.DateTimeField(auto_now_add=True, verbose_name='creado')
     updated = models.DateTimeField(auto_now_add=True, verbose_name='modificado')
 
