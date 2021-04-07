f381219e49f117f5545e2f96b276fbc75bd9ecfc
Pedro Macias <pin.macias@gmail.com>
2021-04-07 20:38:52 +0200

    mostrar los servicios creados en la web

--- refugioApp/views.py
+++ refugioApp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render , HttpResponse
 
+# importar los servicios desde la app servicioApp
+from serviciosApp.models import Servicio
 # Create your views here.
 
 def home(request):
     return render (request,'refugioApp/home.html')
 
 def servicios(request):
-    return render (request,'refugioApp/servicios.html')
+    # en una variable recuperamos todos los objetos que tengo dentro de la clase Servicio
+    servicios = Servicio.objects.all()
+    # incluir los servicios importados
+    return render (request,'refugioApp/servicios.html',{'servicios':servicios})
 
 def refugio(request):
     return render (request,'refugioApp/refugio.html')
