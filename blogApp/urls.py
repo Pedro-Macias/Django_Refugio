c0f54d6170661f41db0ecac3befc308789fa28aa
Pedro Macias <pin.macias@gmail.com>
2021-04-11 11:25:08 +0200

    desarrollo de a app Blog , mostrando vistas y enlaces a categorias

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name='Blog'),
+    path('categoria/<int:categoria_id>/', views.categoria, name='categoria'),
     
 ]
 
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blogApp.models import Post
+from blogApp.models import Post, Categoria
 
 # Create your views here.
 
@@ -8,3 +8,10 @@ def blog(request):
     entradas= Post.objects.all()
     return render (request,'blogApp/blog.html',{'entradas':entradas})
 
+
+def categoria(request, categoria_id):
+    # en una variable recuperamos el id que tengo dentro de la clase categoria
+    categoria = Categoria.objects.get(id=categoria_id)
+    # filtramos los post en base a la categoria
+    entradas= Post.objects.filter(categorias=categoria)
+    return render (request,'blogApp/categoria.html',{'categoria':categoria,'entradas':entradas})
