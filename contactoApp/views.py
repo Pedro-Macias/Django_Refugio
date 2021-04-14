c890c22b115324b98cf78da115bb8425b86e9f40
Pedro Macias <pin.macias@gmail.com>
2021-04-14 21:39:39 +0200

    rescatar información introducida por el usuario en el formulario y mandar mensaje de ok

--- contactoApp/views.py
+++ contactoApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , HttpResponse, redirect
+from django.urls import reverse
 
 # importar el archivo forms
 from .forms import FormContacto
@@ -10,4 +11,15 @@ from .forms import FormContacto
 
 def contacto(request):
     formulario_contacto = FormContacto()
+    # si se ha enviado informacion y es valido
+    if request.method=='POST':
+        formulario_contacto= FormContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get('nombre')
+            email=request.POST.get('email')
+            contenido=request.POST.get('contenido')
+
+            # enviar un parametro para que si todo va bien reciba un mensaje
+            return redirect(reverse('Contacto')+'?ok')          
+
     return render (request,'contactoApp/contacto.html', {'form_Contacto':formulario_contacto})
